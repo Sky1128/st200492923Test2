be2ae2a9e6d8256a0164900101bcaa836b848a60
sky1128 <pa.pruthvish@gmail.com>
2022-02-24 11:15:36 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.st200492923test2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.st200492923test2 to javafx.fxml;
+    exports com.example.st200492923test2;
+}
